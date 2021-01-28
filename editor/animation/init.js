bb0818e1ad63c2a717869c4095aa4ed678412be6
Philippe-Cholet <phcholet@orange.fr>
2021-01-28 19:04:06 +0100

    Update (part 1)

--- editor/animation/init.js
+++ editor/animation/init.js
@@ -1,9 +1,6 @@
-//Dont change it
 requirejs(['ext_editor_io', 'jquery_190'],
     function (extIO, $) {
-        
         var $tryit;
-
         var io = new extIO({
             multipleArguments: false,
             functions: {
--- verification/referee.py
+++ verification/referee.py
@@ -30,49 +30,65 @@ from checkio.referees.io import CheckiOReferee
 
 from tests import TESTS
 
+"""
+@kurosawa4434
+The checker sends (message, list of the problematic cell positions)
+to you for javascript visualization.
+
+You could simply display the problematic cells in red, and show the error
+message if any. If there is an error message but no problematic cell, then the
+grid is fully wrong (types or sizes) and should not be displayed at all.
+"""
+
 
 def checker(input_grid, result):
-    # 1) Check all types.
-    if not (isinstance(result, (tuple, list)) and
-            all(isinstance(row, (tuple, list)) for row in result) and
-            all(isinstance(n, int) for row in result for n in row)):
-        return False, ("The result must be a list/tuple "
-                       "of lists/tuples of ints.")
-    # 2) Check all sizes.
-    nb_rows, nb_cols = len(input_grid), len(input_grid[0])
-    if not (len(result) == nb_rows and
-            all(len(row) == nb_cols for row in result)):
-        return False, "The result must have the same size as input grid."
-    # 3) Check if there is any forbidden change.
-    # And if the contents of the result grid is possible.
-    for i, (row, res_row) in enumerate(zip(input_grid, result)):
-        for j, (n, res_n) in enumerate(zip(row, res_row)):
-            if n and res_n != n:
-                return False, (f"You should not have changed {n} to {res_n} "
-                               f"in the grid at {(i, j)}.")
-            if not (0 < res_n <= 9):
-                return False, f"{res_n} is impossible at {(i, j)}."
-    # 4) Is it filled properly? Thanks to DFS on all connected components.
-    visited = {(i, j): False for i in range(nb_rows) for j in range(nb_cols)}
-    while True:
-        try:
-            start = i, j = next(coord for coord, visit in visited.items()
-                                if not visit)
-        except StopIteration:
-            break
-        stack, nb, count = [start], result[i][j], 0
-        while stack:
-            i, j = stack.pop()
-            visited[i, j] = True
-            count += 1
-            for x, y in ((i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)):
-                if (0 <= x < nb_rows and 0 <= y < nb_cols and
-                        not visited[x, y] and result[x][y] == nb):
-                    stack.append((x, y))
-        if count != nb:
-            return False, (f"Zone at {start} should have {nb} elements, "
-                           f"not {count}.")
-    return True, "Great!"
+    try:
+        # 1) Check types.
+        assert isinstance(result, (tuple, list)) and all(
+            isinstance(row, (tuple, list))
+            and all(isinstance(n, int) and 0 <= n <= 9 for n in row)
+            for row in result
+        ), ('The result must be a list of lists of ints between 0 and 9.', [])
+        # 2) Check sizes.
+        nrows, ncols = len(input_grid), len(input_grid[0])
+        assert (
+            len(result) == nrows and all(len(row) == ncols for row in result)
+        ), ('The result must have the same sizes as the input grid.', [])
+        # 3) Check if there is any forbidden change from the input grid.
+        changes = [(i, j) for i, row in enumerate(input_grid)
+                   for j, n in enumerate(row) if n != 0 and result[i][j] != n]
+        assert not changes, (
+            'You must not change non-empty cells in the given grid.', changes
+        )
+        # 4) Check if there is any empty cell in the result grid.
+        empties = [(i, j) for i, row in enumerate(result)
+                   for j, n in enumerate(row) if n == 0]
+        assert not empties, (
+            'There are %d empty cells.' % len(empties), empties
+        )
+        # 5) Check if the grid is filled the right way.
+        unvisited = {(i, j) for i in range(nrows) for j in range(ncols)}
+        while unvisited:
+            start = i, j = min(unvisited)
+            stack, cells, expected = [start], [], result[i][j]
+            while stack:
+                pos = i, j = stack.pop()
+                if pos not in unvisited:
+                    continue
+                unvisited.remove(pos)
+                cells.append(pos)
+                for neighbor in (i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1):
+                    x, y = neighbor
+                    if 0 <= x < nrows and 0 <= y < ncols:
+                        if neighbor in unvisited and result[x][y] == expected:
+                            stack.append(neighbor)
+            assert len(cells) == expected, (
+                'Zone at %s should have %d elements, not %d.'
+                % (start, expected, len(cells)), sorted(cells)
+            )
+    except AssertionError as error:
+        return False, error.args[0]
+    return True, ('', [])
 
 
 api.add_listener(
@@ -81,8 +97,8 @@ api.add_listener(
         tests=TESTS,
         checker=checker,
         function_name={
-            "python": "filling",
-            # "js": "filling"
+            'python': 'filling',
+            # 'js': 'filling',
         },
-        ).on_ready
+    ).on_ready,
 )
